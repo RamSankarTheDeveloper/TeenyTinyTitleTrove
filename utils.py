fc77c60cf37154d08d233935af63614c82a6083e
ram <ramsankarthedeveloper@gmail.com>
2023-10-02 10:46:07 +0530

    non-robust code. the code works without saving audio files and at a stretch

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ import os
 import spacy
 import re
 import json
+import pyttsx3 #replace it to librosa
+import os
+import librosa
+import numpy as np
+from fastdtw import fastdtw
+from gtts import gTTS
 
 def return_each_files_path(subfolder_path):
     for root, _, files in os.walk(subfolder_path):
@@ -13,12 +19,23 @@ def return_each_files_path(subfolder_path):
 # for path in return_each_files_path(subfolder_path):
 #     print(path)
 
+def extract_data_from_json(file_path):
+    try:
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+    except ValueError as e:
+        print(e)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def filter_entities(doc, label):
     filtered_entities = [ent.text for ent in doc.ents if ent.label_ == label]
     return filtered_entities
-
-
 
 
 def save_as_json(data, file_name, subdirectory):
@@ -63,7 +80,7 @@ def save_as_json(data, file_name, subdirectory):
         print(f"Data saved or appended to {file_path}")
         return file_path  # Return the saved file path
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        print(f"An error occurred in saving json: {str(e)}")
         return None
 
 
@@ -90,8 +107,10 @@ class TextProcessor:
         return file_contents
 
     def clean_text(self, text):
-        cleaned_text = re.sub(r'[^a-zA-Z\s.()\'\";,?]', '', text)
-        cleaned_text = re.sub(r'[&]', 'and', cleaned_text)
+        cleaned_text = re.sub(r'\n', '. ', text)
+        #cleaned_text = re.sub(r'[^\n]', '. ', text)
+        cleaned_text = re.sub(r'[&]', ' and ', cleaned_text)
+        cleaned_text = re.sub(r'[^a-zA-Z.();,?\'\"]', ' ', cleaned_text)
         return cleaned_text
 
     def tokenize_text(self, text):
@@ -113,8 +132,68 @@ class TextProcessor:
         # doc_of_each_chunk=[self.tokenize_text(each_chunk) for each_chunk in book_chunks]
         # return doc_of_each_chunk
     
-        for i in range(0, len(cleaned_text), self.chunksize):           
+        for i in range(0, len(cleaned_text), self.chunksize):         
             text_chunk = cleaned_text[i:i + self.chunksize]
+            
+                
             yield text_chunk
 
 
+class WordsComparer():
+    def __init__(self, word='',word2='') -> None:
+        self.word = word.lower()
+        self.word2= word2.lower()
+        self.file_name = f"{self.word}.wav"
+        # if is_sample_name == True:
+        #     self.destination_path = os.path.join(r"data\audio_data\sample_audio_data", self.file_name)# change to join format for cross platform compatibility
+        # if is_sample_name == False:
+        #     self.destination_path = os.path.join(r"data\audio_data\doc_audio_data", self.file_name)
+
+    def convert_to_audio(self, word):
+        file_name = f"{word}.wav"
+        
+        # Create an instance of the gTTS class
+        tts = gTTS(word)
+
+        # Save the audio file as a temporary file
+        tts.save(f"{word}.wav")
+        
+
+        return file_name
+
+    def audio_to_mfcc(self, audio_file_path):
+
+        # Load the audio files #r"data\audio_data\sample_audio_data\output_audio1.wav"
+
+        audio, sr_doc = librosa.load(audio_file_path, sr= None)
+        print("sr",sr_doc)
+
+        # Extract MFCC features
+        mfcc_doc = librosa.feature.mfcc(y= audio, sr= sr_doc)
+
+        # Transpose MFCCs for compatibility with DTW
+        mfcc_doc = mfcc_doc.T
+        return mfcc_doc
+
+    def compare_two_MFCC(self,mfcc1,mfcc2):  #get more information about this
+        def calculate_distance(mfcc1,mfcc2):
+            # Calculate DTW distance and path
+            distance, _ = fastdtw(mfcc1, mfcc2)
+            return distance
+        distance = calculate_distance(mfcc1,mfcc2)
+        # Normalize the distance (optional)
+        normalised_distance = distance / (len(mfcc1) + len(mfcc2))
+        normalised_distance = round(normalised_distance,2)
+        return normalised_distance
+
+    def word_to_mfcc(self, word):
+        file_name = self.convert_to_audio(word)
+        mfcc_file = self.audio_to_mfcc(file_name)
+        os.remove(file_name)
+        return mfcc_file
+
+    def process(self):
+        mfcc1= self.word_to_mfcc(self.word)
+        mfcc2= self.word_to_mfcc(self.word2)
+        distance= self.compare_two_MFCC(mfcc1=mfcc1, mfcc2=mfcc2)
+        return distance
